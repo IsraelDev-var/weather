fc84eea09e75006835e7b9423b6be67cc65a9ab6
Israel <israel230804vc@gmail.com>
2023-08-09 16:42:28 -0400

    the weather App 2.4

--- src/assets/components/DarkMode.jsx
+++ src/assets/components/DarkMode.jsx
@@ -3,7 +3,7 @@ import { useState } from "react"
 const DarkMode = () => {
 const [isdark, setIsdark] = useState(true)
    const  handleDarkMode = () =>{
-    if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    if (!localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
        
     } else {
        
@@ -11,30 +11,22 @@ const [isdark, setIsdark] = useState(true)
         
     }
     
-    
-    localStorage.theme = 'light'
-    
-    
-    localStorage.theme = 'dark'
-    
-    
-    localStorage.removeItem('theme')
     }
-    
-
 return (
     <>
-    <button onClick={handleDarkMode}>
+    
+
+        
+        <button onClick={handleDarkMode}  className="   animate-bounce bg-[rgb(4,115,171)] 100%)] btn-change hover:bg-cyan-600 dark:text-black  p-2 rounded-2 border-1 border-neutral-400 text-ms hover:text-white"  type="button" >
+
         {
             isdark ? 
-            <i className="font-bold text-5xl text-black dark:text-neutral-950 cursor-pointer" class=' text-4xl bx bxs-moon text-black' ></i>
+            <i   class="font-bold  dark:text-neutral-950 cursor-pointer text-4xl bx bxs-moon text-black" ></i>
             :
-            <i className="  font-bold text-5xl" class='bx bxs-sun text-4xl text-yellow-500 '  ></i>
+            <i  class="  font-bold bx bxs-sun text-4xl text-yellow-500 "  ></i>
             
         }
-        
-        
-    </button>
+        </button>
     
     </>
 )
